033a91810c543b19d59051b1179aba9ce2576ee3
Djekson <alekssyvorov29@gmail.com>
2023-06-26 22:09:36 +0300

    4

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def func():
-    pass
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run()
